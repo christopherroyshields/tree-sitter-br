b5507af96b92903d7cdb040f0d33898a48f9fe92
Chris <christopherroyshields@gmail.com>
2023-01-25 09:54:47 -0600

    made reread parameters optional

--- grammar.js
+++ grammar.js
@@ -1471,7 +1471,7 @@ module.exports = grammar({
     reread_statement: $ => seq(
       STATEMENTS.reread,
       $.channel,
-      choice(
+      optional(
         seq(
           ",",
           choice(
